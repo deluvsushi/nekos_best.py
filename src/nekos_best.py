79c13095d8caa847cee0ff6c6edef666d1ea4ffb
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-08-21 20:45:37 +0600

    Create nekos_best.py

---
+++ src/nekos_best.py
@@ -0,0 +1,52 @@
+from os import getcwd
+from time import time
+from pathlib import Path
+from requests import get
+
+
+class NekosBest:
+	def __init__(self):
+		self.api = "https://nekos.best/api/v2"
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+		}
+	
+	def save_file(
+			self,
+			content: bytes,
+			location: str = getcwd()):
+		with open(
+			Path(location).joinpath(f"{time() * 1000}.jpg"),
+		mode="wb+",
+		) as file:
+			file.write(content)
+			file.close()
+		return True
+
+	def get_endpoints(self):
+		return get(
+			f"{self.api}/endpoints",
+			headers=self.headers).json()
+	
+	def get_random_image(self, category: str, amount: int = 1):
+		return get(
+			f"{self.api}/{category}?amount={amount}",
+			headers=self.headers).json()
+	
+	def search_for_phrase(
+			self,
+			query: str,
+			category: str,
+			amount: int = 1):
+		return get(
+			f"{self.api}/search?query={query}&type={type}&category={category}&amount={amount}",
+			headers=self.headers).json()
+			
+	def get_file(
+			self,
+			category: str,
+			file_name: str,
+			format: str):
+		return self.save_file(get(
+			f"{self.api}/{category}/{file_name}.{format}",
+			headers=self.headers).content)
